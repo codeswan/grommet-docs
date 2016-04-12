05aa5f740b936d9212796f7eebc98cabc4b9f5be
Allison Day <day.allison@gmail.com>
2016-04-11 17:51:02 -0700

    Naming fix
    Signed-off-by: Allison Day <day.allison@gmail.com>

--- src/develop/components/FactoidDoc.js
+++ src/develop/components/FactoidDoc.js
@@ -33,15 +33,15 @@ var FactoidDoc = React.createClass({
 
   render: function() {
     var simpleFactoid = (
-      <Factoid data="10,000" caption="Sample Factoid" />
+      <Factoid text="10,000" caption="Sample Factoid" />
     );
 
     var smallFactoid = (
-      <Factoid size="small" iconBefore="Assistant" data="100%" iconAfter="Achievement" caption="Teeny tiny Factoid with icons" />
+      <Factoid size="small" iconBefore="Assistant" text="100%" iconAfter="Achievement" caption="Teeny tiny Factoid with icons" />
     );
 
     var bigFactoid = (
-      <Factoid size="large" data="1000%" iconAfter="LinkUp" caption="Huge Factoid" />
+      <Factoid size="large" text="1000%" iconAfter="LinkUp" caption="Huge Factoid" />
     );
 
     return (
@@ -55,12 +55,12 @@ var FactoidDoc = React.createClass({
           <dl>
             <dt><code>size            small|medium|large</code></dt>
             <dd>The size of the Factoid. Defaults to <code>medium</code>.</dd>
-            <dt><code>data            string</code></dt>
+            <dt><code>text            string</code></dt>
             <dd>The very large number.  This parameter is required</dd>
             <dt><code>iconBefore      string</code></dt>
-            <dd>Name of the icon to go before the data.  See <Link to={this.context.routePrefix + "icon"}>Icon</Link> for available options.  This parameter is optional.</dd>
+            <dd>Name of the icon to go before the text.  See <Link to={this.context.routePrefix + "icon"}>Icon</Link> for available options.  This parameter is optional.</dd>
             <dt><code>iconAfter       string</code></dt>
-            <dd>Name of the icon to go after the data.  See <Link to={this.context.routePrefix + "icon"}>Icon</Link> for available options.  This parameter is optional.</dd>
+            <dd>Name of the icon to go after the text.  See <Link to={this.context.routePrefix + "icon"}>Icon</Link> for available options.  This parameter is optional.</dd>
             <dt><code>caption         string</code></dt>
             <dd>Caption for the factoid.  Optional.</dd>
           </dl>
